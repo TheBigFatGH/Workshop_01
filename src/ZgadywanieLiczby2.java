76fb0b8019812b875e95819bc0e97bc2af61cc5b
Tom <tomala.tomasz@gmail.com>
2020-05-17 18:35:20 +0200

    Zgadywanie2

--- src/ZgadywanieLiczby2.java
+++ src/ZgadywanieLiczby2.java
@@ -6,16 +6,16 @@ public class ZgadywanieLiczby2 {
         System.out.println("Pomysl liczne od 0 do 1000, a ja ją zgadnę w max. 10 próbach");
         int min = 0;
         int max = 1000;
-        boolean win = false;
+        boolean playagian = true;
         int count = 1;
         int guess;
 
 
-        while (!win) {
-            if (count > 10) {
+        while (playagian) {
+            if (count == 11) {
                 System.out.println("Pewnie oszukiwales");
-                win = true;
                 break;
+
             }
             guess = ((max - min) / 2) + min;
             System.out.println("Proba " + count);
@@ -27,7 +27,7 @@ public class ZgadywanieLiczby2 {
 
             if ("zgadles".equals(answer)) {
                 System.out.println("Wygrałem");
-                win = true;
+                playagian = false;
             } else if ("za duzo".equals(answer)) {
                 max = guess;
                 count++;
